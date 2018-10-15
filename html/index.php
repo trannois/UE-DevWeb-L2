b75dc3330dec3a0c25cf7124e7996c88a4eb6a4e
trannois <harold.trannois@u-picardie.fr>
2018-10-15 10:06:10 +0200

    Solution TP2 exo2 Step #1

---
+++ html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Requete</title>
+</head>
+<body>
+
+<div class="container-fluid">
+    <h3>Timer</h3>
+    <hr>
+
+    <button onclick="document.getElementById('indicateur').innerText='M';"> Démarrer</button>
+    <i id="indicateur"></i>
+    <button onclick="document.getElementById('indicateur').innerText='A';">Arrêter</button>
+
+</div>
+</body>
+</html>
