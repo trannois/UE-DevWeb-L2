8d16252e5451123660de671820f7e9dcebb5b0a0
trannois <harold.trannois@u-picardie.fr>
2019-09-12 17:08:58 +0200

    Affiche des coordonnées GPS de la ville du client (porter l'attention sur la pertinance d'une clef étrangère )

--- html/listeFromJson.php
+++ html/listeFromJson.php
@@ -1,7 +1,30 @@
 <?php
+// lit tous les clients à partir du fichier de donnée en JSON
 $contientLesDonneeDuFichier = file_get_contents(__DIR__."/../data/clients.json");
 $toutMesClients = json_decode($contientLesDonneeDuFichier, true);
 $nbrClients = sizeof($toutMesClients);
+
+// lit les informations sur toutes les villes à partir d'un fichier JSON
+// source : https://github.com/high54/Communes-France-JSON.git
+$villeJson = file_get_contents(__DIR__."/../data/france.json");
+$villes = json_decode($villeJson, true);
+
+/**
+ * Recherche les infos d'une ville dans le tableau des villes à partir de son code
+ * Recherche en O(n)
+ * @param $code
+ * @return |null
+ */
+function rechercheVille( $code ) {
+    global  $villes, $nbrVilles;
+    foreach ( $villes as $ville ) {
+        if ( $ville['Code_commune_INSEE'] == $code ) {
+            return $ville;
+        }
+    }
+    return null;
+}
+
 ?>
 <table border="1">
 
@@ -10,6 +33,9 @@ $nbrClients = sizeof($toutMesClients);
      for( $i=0; $i<$nbrClients; $i=$i+1) {
          print "<tr><td>";
          print $toutMesClients[$i]["nom"];
+         print "</td><td>";
+         $villeClient = rechercheVille( $toutMesClients[$i]["ville"] );
+         print( $villeClient["coordonnees_gps"] );
          print "</td></tr>";
      }
     ?>
